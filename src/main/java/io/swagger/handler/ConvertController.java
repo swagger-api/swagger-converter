febad3cbc5307032a75e9e316d9f12c2fa77ad49
fehguy <fehguy@gmail.com>
2017-04-07 14:30:23 +0000

    Generated code by SwaggerHub

---
+++ src/main/java/io/swagger/handler/ConvertController.java
@@ -0,0 +1,29 @@
+package io.swagger.handler;
+
+import io.swagger.inflector.models.RequestContext;
+import io.swagger.inflector.models.ResponseContext;
+import javax.ws.rs.core.Response.Status;
+
+import org.glassfish.jersey.media.multipart.FormDataContentDisposition;
+import java.io.File;
+import java.util.List;
+
+import io.swagger.model.*;
+
+
+@javax.annotation.Generated(value = "class io.swagger.codegen.languages.JavaInflectorServerCodegen", date = "2017-04-07T14:30:24.843Z")
+public class ConvertController  {
+  /** 
+   * Uncomment and implement as you see fit.  These operations will map
+   * Directly to operation calls from the routing logic.  Because the inflector
+   * Code allows you to implement logic incrementally, they are disabled.
+   **/
+
+  /*
+    public ResponseContext convertPost(RequestContext request , Object inputSpec, String url) {
+        return new ResponseContext().status(Status.INTERNAL_SERVER_ERROR).entity( "Not implemented" );
+    }
+  */
+
+}
+
