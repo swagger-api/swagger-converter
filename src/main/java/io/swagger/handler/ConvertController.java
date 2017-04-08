55c246cb7a235637dedb1aaada35229f5f6fe361
Tony Tam <fehguy@gmail.com>
2017-04-08 08:52:45 -0700

    separated methods

--- src/main/java/io/swagger/handler/ConvertController.java
+++ src/main/java/io/swagger/handler/ConvertController.java
@@ -1,35 +1,48 @@
 package io.swagger.handler;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import io.swagger.inflector.models.RequestContext;
 import io.swagger.inflector.models.ResponseContext;
-import javax.ws.rs.core.Response.Status;
+import io.swagger.parser.OpenAPIParser;
+import io.swagger.parser.models.SwaggerParseResult;
 
-import org.glassfish.jersey.media.multipart.FormDataContentDisposition;
-import java.io.File;
-import java.util.List;
-
-import io.swagger.model.*;
-
+import javax.ws.rs.core.Response;
 
 @javax.annotation.Generated(value = "class io.swagger.codegen.languages.JavaInflectorServerCodegen", date = "2017-04-08T15:48:56.501Z")
 public class ConvertController  {
-  /** 
-   * Uncomment and implement as you see fit.  These operations will map
-   * Directly to operation calls from the routing logic.  Because the inflector
-   * Code allows you to implement logic incrementally, they are disabled.
-   **/
-
-  /*
-    public ResponseContext convertByContent(RequestContext request , Object inputSpec) {
-        return new ResponseContext().status(Status.INTERNAL_SERVER_ERROR).entity( "Not implemented" );
+    public ResponseContext convertByContent(RequestContext request , JsonNode inputSpec) {
+            if(inputSpec == null) {
+                return new ResponseContext()
+                        .status(Response.Status.BAD_REQUEST)
+                        .entity( "No specification supplied in either the url or request body.  Try again?" );
+            }
+            String inputAsString = inputSpec.toString();
+            SwaggerParseResult output = new OpenAPIParser().readContents(inputAsString, null, null);
+            if(output == null) {
+                return new ResponseContext().status(Response.Status.INTERNAL_SERVER_ERROR).entity( "Failed to process URL" );
+            }
+            if(output.getOpenAPI() == null) {
+                return new ResponseContext().status(Response.Status.BAD_REQUEST).entity(output.getMessages());
+            }
+            return new ResponseContext()
+                    .entity(output.getOpenAPI());
     }
-  */
 
-  /*
     public ResponseContext convertByUrl(RequestContext request , String url) {
-        return new ResponseContext().status(Status.INTERNAL_SERVER_ERROR).entity( "Not implemented" );
+        if(url == null) {
+            return new ResponseContext()
+                    .status(Response.Status.BAD_REQUEST)
+                    .entity( "No specification supplied in either the url or request body.  Try again?" );
+        }
+        SwaggerParseResult output = new OpenAPIParser().readLocation(url, null, null);
+        if(output == null) {
+            return new ResponseContext().status(Response.Status.INTERNAL_SERVER_ERROR).entity( "Failed to process URL" );
+        }
+        if(output.getOpenAPI() == null) {
+            return new ResponseContext().status(Response.Status.BAD_REQUEST).entity(output.getMessages());
+        }
+        return new ResponseContext()
+                .entity(output.getOpenAPI());
     }
-  */
-
 }
 
